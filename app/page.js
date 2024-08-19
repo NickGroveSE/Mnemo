1af52cb33034cd585624451ceeaa59b1895ac485
Zutzut <zainab4787@gmail.com>
2024-08-19 15:05:16 -0500

    got gemini generating

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { firestore } from '@/firebase';
+import { firestore } from '../firebase';
 import { Container, Box, Typography, AppBar, Toolbar} from '@mui/material'
 
 export default function Home() {
